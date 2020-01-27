aba68ad0cf70986eb6446c0891258b50258c5d38
Craz3Squirr3l <mstein999@gmail.com>
2020-01-26 20:25:38 -0500

    Finished iterationStatements

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// using while statements and other iterative methods
+
+using System;
+using static System.Console;
 
 namespace IterationStatements
 {
@@ -6,7 +9,51 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0;
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+
+            string password = string.Empty;
+            // int i = 0;
+            do
+            {
+                Write("Enter your password: ");
+                password = ReadLine();
+                // i++;
+            } while (password != "Pa$$w0rd" && i < 10);
+            // } while (password != "Pa$$w0rd" && i< 10);
+            // if (i >= 10)
+            // {
+            //     WriteLine("Too many attempts");
+            // }
+            // else
+            // {
+            //     WriteLine("Correct!");
+            // }
+
+
+            // using for statments
+            for (int y = 0; y <= 10; y++)
+            {
+                WriteLine(y);
+            }
+
+            // foreach statement
+            string[] names = { "Adam", "Barry", "Charlie" };
+            foreach (string name in names)
+            {
+                WriteLine($"{name} has {name.Length} characters");
+            }
+            // Foreach is similar to the following pseudocode:
+            // IEnumerator e = names.GetEnumerator();
+            // while (e.MoveNext())
+            // {
+            //     string name = (string)e.Current; // Current is read-only!
+            //     WriteLine($"{name} has {name.Length} characters.");
+            // }
         }
     }
 }
