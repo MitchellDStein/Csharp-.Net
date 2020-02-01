f5c06f5afb7d927779c9fd2b26182fe35bdbd294
Craz3Squirr3l <mstein999@gmail.com>
2020-02-01 01:21:54 -0500

    Finished Chapter 3

---
+++ Chapter03/Exercise3.4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Asks the user for two numbers in the range 0-255
+// and then divides the first number by the second3
+using System;
+using static System.Console;
+
+namespace Exercise3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a number from 0 to 255: ");
+            string firstInput = ReadLine();
+            Write("Enter another number between 0 and 255: ");
+            string secondInput = ReadLine();
+
+            try
+            {
+                byte firstInt = byte.Parse(firstInput);
+                byte secondInt = byte.Parse(secondInput);
+                WriteLine($"{firstInt} divided by {secondInt} is {firstInt/secondInt}");
+            }
+            catch (FormatException ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+                return;
+            }
+            catch (OverflowException ex)
+            {
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+                return;
+            }
+        }
+    }
+}
---
+++ Chapter03/Exercise3.5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // What are the values of x and y after the following statements execute?
+
+            // x = 3
+            // y = 2 + ++x
+            // A: x = 4, y = 6
+            // Check:
+            int x = 3;
+            int y = 2 + ++x;
+            WriteLine($"#1: x = {x}, y = {y}");
+
+
+            // x = 3 << 2;  - bitwise shift left
+            // y = 10 >> 1; - bitwise shift right
+            // 3 = 0000 0011, 10 = 0000 1010
+            // A: 3 << 2 = 0000 1100 = 12,   10 >> 1 = 0000 0101 = 5
+            // Check:
+            x = 3 << 2;
+            y = 10 >> 1;
+            WriteLine($"#2: 3 << 2 = {x}");
+            WriteLine($"    10 >> 1 = {y}");
+
+            // x = 10 & 8;  - AND operator  (both are 1's)
+            // y = 10 | 7;  - OR operator   (any are 1's)
+            //  10 = 0000 1010     |  10 = 0000 1010
+            //   8 = 0000 1000     |   7 = 0000 0111
+            // AND = 0000 1000 = 8 |  OR = 0000 1111 = 15
+            // Check:
+            x = 10 & 8;
+            y = 10 | 7;
+            WriteLine($"#3: 10 & 8 = {x}");
+            WriteLine($"    10 | 7 = {y}");
+        }
+    }
+}
